e1254323ca0bfbbdf6597878b2323de28746822f
Thamizhselvan <tamildharani51@gmail.com>
2026-04-14 20:24:32 +0530

    Database Linked

--- AI_Prep/settings.py
+++ AI_Prep/settings.py
@@ -80,11 +80,13 @@ WSGI_APPLICATION = 'AI_Prep.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
+import dj_database_url
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.parse(
+        "postgresql://postgres:thamizh@098@db.lfxtupvpuhshbzxfbqii.supabase.co:5432/postgres",
+        conn_max_age=600
+    )
 }
 
 
